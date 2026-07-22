59919ad936101b61e41edfc9cfffff8eef9faa21
JesusVA760 <tonio4340406@gmail.com>
2026-07-21 23:25:50 -0700

    Complete project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,65 @@
-import Hero from "./Hero";
-import Projects from "./Projects";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import {
+  About,
+  HomeLayout,
+  Landing,
+  Error,
+  Newsletter,
+  Cocktail,
+  SinglePageError,
+} from "./pages";
+
+import { loader as landingLoader } from "./pages/Landing";
+import { loader as singleCocktailLoader } from "./pages/Cocktail";
+import { action as newsletterAction } from "./pages/Newsletter";
+
+const queryCLient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+});
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+        loader: landingLoader(queryCLient),
+        errorElement: <SinglePageError />,
+      },
+      {
+        path: "/cocktail/:id",
+        errorElement: <SinglePageError />,
+        loader: singleCocktailLoader(queryCLient),
+        element: <Cocktail />,
+      },
+      {
+        path: "/newsletter",
+        element: <Newsletter />,
+        action: newsletterAction,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+    ],
+  },
+]);
 
 const App = () => {
   return (
-    <main>
-      <Hero />
-      <Projects />
-    </main>
+    <QueryClientProvider client={queryCLient}>
+      <RouterProvider router={router} />;
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 export default App;
